6b5b68f39b898414a041ad911698e549ab6dee5a
Daniel Pozzi <daniel.pozzi@leanix.net>
2015-09-04 09:38:16 +0200

    added build file (can be used instead of post-commit hook)

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "gh-pages" == "$branch" ]; then
+    exit
+fi
+
+echo Updating gh-pages for branch $branch
+
+git checkout gh-pages
+git checkout $branch -- dist
+mkdir -p $branch
+git rm --ignore-unmatch -rf $branch
+mv -f dist/* $branch
+git rm -rf --ignore-unmatch dist
+git add $branch
+git describe --always | git commit -m -
+git checkout $branch
